b7d0a4cdeeebdafcdfa1a16e40b205e4a4aeab28
lucjastozek <lucjastozek@gmail.com>
2023-07-24 15:18:58 +0200

    setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import { MyComponent } from "./MyComponent";
 import "./App.css";
 import { greet } from "../greet";
+import episodes from "../episodes.json";
 
 function App() {
+    console.log(`Imported ${episodes.length} episode(s)`);
+    console.log(`First episode's name is ${episodes[0].name}`);
     return (
         <div className="App">
             <MyComponent />
