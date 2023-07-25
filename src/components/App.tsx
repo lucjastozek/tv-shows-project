c2558ae9171074db9b5334ad4cb9e79017ca4908
Ana Rebelo <anagmrebelo@hotmail.com>
2023-07-25 12:18:12 +0200

    Add search bar without filtering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import episodes from "../episodes.json";
 import { EpisodeList } from "./EpisodeList";
 import { PageHeader } from "./PageHeader";
 import { PageFooter } from "./PageFooter";
+import { SearchBar } from "./SearchBar";
 
 function App() {
+    const [inpVal, setInpVal] = useState("");
+
     return (
         <div className="App">
             <PageHeader />
+            <SearchBar inpVal={inpVal} setInpVal={setInpVal} />
             <EpisodeList episodes={episodes} />
             <PageFooter />
         </div>
