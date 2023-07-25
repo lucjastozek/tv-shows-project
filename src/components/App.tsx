3687e3399e55495141826cf73d36a16e8f5638ef
Ana Rebelo <anagmrebelo@hotmail.com>
2023-07-25 12:52:56 +0200

    Implement filter functionality for search bar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,28 @@
 import "./App.css";
-import { useState } from "react";
 import episodes from "../episodes.json";
+import { useState } from "react";
 import { EpisodeList } from "./EpisodeList";
 import { PageHeader } from "./PageHeader";
 import { PageFooter } from "./PageFooter";
 import { SearchBar } from "./SearchBar";
+import { IEpisode } from "../utils/IEpisode";
 
 function App() {
     const [inpVal, setInpVal] = useState("");
+    const [displayedEpisodes, setDisplayedEpisodes] =
+        useState<IEpisode[]>(episodes);
 
     return (
         <div className="App">
             <PageHeader />
-            <SearchBar inpVal={inpVal} setInpVal={setInpVal} />
-            <EpisodeList episodes={episodes} />
+            <SearchBar
+                inpVal={inpVal}
+                setInpVal={setInpVal}
+                displayedEpisodes={displayedEpisodes}
+                setDisplayedEpisodes={setDisplayedEpisodes}
+                listOfAllEpisodes={episodes}
+            />
+            <EpisodeList episodes={displayedEpisodes} />
             <PageFooter />
         </div>
     );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,20 @@
+import { IEpisode } from "../utils/IEpisode";
+import { filterEpisodes } from "../utils/filterEpisodes";
+
 interface SearchBarProps {
     inpVal: string;
     setInpVal: React.Dispatch<React.SetStateAction<string>>;
+    displayedEpisodes: IEpisode[];
+    setDisplayedEpisodes: React.Dispatch<React.SetStateAction<IEpisode[]>>;
+    listOfAllEpisodes: IEpisode[];
 }
 
-function SearchBar({ inpVal, setInpVal }: SearchBarProps): JSX.Element {
+function SearchBar({
+    inpVal,
+    setInpVal,
+    setDisplayedEpisodes,
+    listOfAllEpisodes,
+}: SearchBarProps): JSX.Element {
     return (
         <>
             <input
@@ -12,6 +23,9 @@ function SearchBar({ inpVal, setInpVal }: SearchBarProps): JSX.Element {
                 value={inpVal}
                 onChange={(e) => {
                     setInpVal(e.target.value);
+                    setDisplayedEpisodes(
+                        filterEpisodes(inpVal, listOfAllEpisodes)
+                    );
                 }}
             ></input>
         </>
