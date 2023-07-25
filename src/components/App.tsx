447a18e037090dc22b8edc749a028a1b7b642d70
lucjastozek <lucjastozek@gmail.com>
2023-07-25 11:57:26 +0200

    add footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import episodes from "../episodes.json";
 import { EpisodeList } from "./EpisodeList";
 import { PageHeader } from "./PageHeader";
+import { PageFooter } from "./PageFooter";
 
 function App() {
     return (
         <div className="App">
             <PageHeader />
             <EpisodeList episodes={episodes} />
+            <PageFooter />
         </div>
     );
 }
