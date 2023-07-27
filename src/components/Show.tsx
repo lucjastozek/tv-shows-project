53b6d0fcee1949c5b411fe685dbd24d529723def
arebelo <anagmrebelo@hotmail.com>
2023-07-27 13:59:18 +0200

    Add structure of Show component

---
+++ src/components/Show.tsx
@@ -0,0 +1,18 @@
+// When your app starts, present a listing of all shows ("shows listing")
+// For each show, you must display at least name, image, summary, genres, status, rating, and runtime.
+
+function Show(): JSX.Element {
+    return (
+        <>
+            <h3>Name</h3>
+            <img />
+            <p>Lorem ipsum</p>
+            <p>genres</p>
+            <p>status</p>
+            <p>rating</p>
+            <p>runtime</p>
+        </>
+    );
+}
+
+export { Show };
